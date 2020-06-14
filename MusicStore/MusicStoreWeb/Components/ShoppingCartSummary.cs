1528202547bc902e5b6a70cce300c099b568721c
Daniel Kondow <danielkondov91@gmail.com>
2020-06-14 23:59:37 +0300

    Shopping Cart UI Component

---
+++ MusicStore/MusicStoreWeb/Components/ShoppingCartSummary.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicStoreWeb.Infrastructure.Extensions;
+using MusicStoreWeb.Services.Contracts;
+using System.Linq;
+
+namespace MusicStoreWeb.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly IShoppingCartManager shoppingCartManager;
+
+        public ShoppingCartSummary(IShoppingCartManager shoppingCartManager)
+        {
+            this.shoppingCartManager = shoppingCartManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var shoppingCartId = this.HttpContext.Session.GetShoppingCartId();
+
+            var items = this.shoppingCartManager.GetItems(shoppingCartId);
+
+            var quantity = items.Sum(i => i.Quantity);
+
+            return View(quantity);
+        }
+    }
+}
